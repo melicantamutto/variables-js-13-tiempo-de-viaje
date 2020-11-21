af407172d8a057c31e6102ee35e3488916319dad
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:04:12 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Tiempo de viaje');
+
+alert('Tiempo de viaje');
+
+const pie = 5;
+const bici = 10;
+const auto = 80;
+
+const distancia = prompt('Ingresa la distancia del recorrido en km');
+
+alert(`Su viaje de ${distancia}km le tomará:
+    A pie: ${distancia/pie} horas,
+    En bicicleta: ${distancia/bici} horas,
+    Y en auto: ${distancia/auto} horas.
+`);
